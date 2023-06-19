042afda4b000fe029128171ba7f2c9e50ddb41ff
Ryan <r.f.hylands@gmail.com>
2023-06-19 18:37:02 +0100

    songs

--- guitarBE/src/api/song/controllers/song.ts
+++ guitarBE/src/api/song/controllers/song.ts
@@ -4,4 +4,139 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::song.song');
+export default factories.createCoreController('api::song.song', ({ strapi }) => ({
+
+    async getOne(ctx) {
+
+        let entries = await strapi.entityService.findMany('api::song.song', {
+            fields: ['title', 'artist', 'status'],
+            filters: {
+                owner: ctx.state.user.id,
+                id: ctx.params.id
+            },
+            sort: { artist: 'DESC' }
+        });
+
+        return entries;
+
+    },
+
+    async delete(ctx){
+   
+        const id = ctx.params.id;
+
+        const entries = await strapi.entityService.findMany('api::song.song', {
+            filters: {
+                owner: ctx.state.user.id,
+                id: id
+            },
+        });
+
+
+
+
+        if(entries.length === 0){
+            return
+        }
+
+        const d = await strapi.entityService.delete('api::song.song', id)
+
+        return d;
+
+    
+    },
+
+    async getMany(ctx) {
+        const query = ctx.request.query;
+        const start = query.start ?? 0;
+        const limit = query.limit ?? 25;
+
+
+        let filters:any = { 
+            owner: ctx.state.user.id,
+         };
+
+         if(query.status){
+            filters.status = query.status;
+         }
+
+        let entries = await strapi.entityService.findMany('api::song.song', {
+            fields: ['title', 'artist', 'status'],
+            filters,
+            sort: { artist: 'DESC' },
+            start,
+            limit
+        });
+
+
+        return entries;
+    },
+
+    async create(ctx) {
+
+        const body = ctx.request.body;
+
+        strapi.log.debug(JSON.stringify(body))
+        strapi.log.debug('status')
+        strapi.log.debug(body.status)
+
+        let entry;
+        try {
+
+        
+        entry = await strapi.entityService.create('api::song.song', {
+            data: {
+                title: body.song,
+                artist: body.artist,
+                status: body.status,
+                owner: {
+                    set: [ctx.state.user.id]
+                }
+            },
+        });
+    } catch (e){
+
+    }
+
+
+        return entry;
+
+    },
+
+    async update(ctx) {
+
+        const id = ctx.params.id;
+        const body = ctx.request.body;
+
+        const entries = await strapi.entityService.findMany('api::song.song', {
+            filters: {
+                owner: ctx.state.user.id,
+                id: id
+            },
+        });
+
+        if(entries.length === 0){
+            return
+        }
+
+
+
+        const entry = await strapi.entityService.update('api::song.song', id, {
+            data: {
+                title: body.song || entries.title,
+                artist: body.artist || entries.artist,
+                status: body.status || entries.status,
+            },
+        });
+
+        strapi.log.debug(JSON.stringify(entry))
+
+        return entry;
+
+
+
+    }
+
+
+}));
+
--- guitarBE/src/api/song/routes/song.ts
+++ guitarBE/src/api/song/routes/song.ts
@@ -2,6 +2,34 @@
  * song router
  */
 
-import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::song.song');
+
+export default {
+    routes: [
+        { // Path defined with a URL parameter
+          method: 'GET',
+          path: '/song/',
+          handler: 'song.getMany',
+        },
+        { // Path defined with a URL parameter
+          method: 'DELETE',
+          path: '/song/:id',
+          handler: 'song.delete',
+        },
+        { // Path defined with a URL parameter
+            method: 'GET',
+            path: '/song/:id',
+            handler: 'song.getOne',
+          },
+        {
+            method: 'POST',
+            path: '/song/',
+            handler: 'song.create',
+        },
+        {
+          method: 'POST',
+          path: '/song/:id',
+          handler: 'song.update',
+      }
+    ]
+}
